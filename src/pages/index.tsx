d00e3c9b2aa7d4723d09859346de86582dedef74
aldev1010 <aldev1010@gmail.com>
2022-07-05 20:13:23 +0200

    chore: init

---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from "next";
+import { signIn, signOut } from "next-auth/react";
+import React from "react";
+import Heading from "../ui/layout/components/Heading";
+import PageLayout from "../ui/layout/components/PageLayout";
+import MyUserAvatar from "../ui/user/components/MyUserAvatar";
+import { useMe } from "../ui/user/hooks/useMe";
+
+const Home: NextPage = () => {
+  const me = useMe().data?.me;
+
+  return (
+    <PageLayout>
+      <Heading>Starter Project</Heading>
+      {me ? (
+        <div>
+          <p>hello {me.email}</p>
+          <MyUserAvatar />
+          <button onClick={() => signOut({ redirect: false })}>Signout </button>
+        </div>
+      ) : (
+        <button onClick={() => signIn("google")}>Sign in with Google</button>
+      )}
+    </PageLayout>
+  );
+};
+
+export default Home;
